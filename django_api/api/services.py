75fbc1354e3c1cdd85ee2314dacd8c8abde7205b
t-supero.bruno <t-supero.bruno@nexxera.com>
2019-07-08 16:53:57 -0300

    Calculate bills

--- django_api/api/services.py
+++ django_api/api/services.py
@@ -13,11 +13,26 @@ class ApiService:
         standing_charge = 0.36
         call_charge = 0.09
 
-        ini = self.start['record_timestamp']
-        yourdate = dateutil.parser.parse(ini)
+        start_hour = dateutil.parser.parse(self.start['record_timestamp'])
+        stop_hour = dateutil.parser.parse(self.stop['record_timestamp'])
         # a = datetime.strptime(yourdate, '%Y-%m-%d %H:%M:%S')
-        b = yourdate.hour
-        # if self.start.hour > 6 and self.stop.hour < 22:
+        # b = yourdate.hour
+        if start_hour.hour > 6 and stop_hour.hour <= 22:
+            print("X")
+            if stop_hour.hour == 22:
 
+                stop_date = datetime.strptime(self.stop['record_timestamp'], "%Y-%m-%dT%H:%M:%SZ").replace(minute=00)
+
+            seconds = (stop_hour - start_hour).total_seconds()
+            minutes = seconds/60
+
+            price = minutes * call_charge + standing_charge
+        elif start_hour.hour > 22 and stop_hour.hour < 6:
+            print("Y")
+            seconds = (stop_hour - start_hour).total_seconds()
+            minutes = seconds / 60
+            price = 0.36
+        else:
+            print("Z")
 
         return 'ok'
