7067d937d598e39ece0fc237b1c35366a19be8d7
mz5806 <elyson.silva@supero.com.br>
2019-08-06 22:36:42 -0300

    Improve call bills

--- django_api/api/views.py
+++ django_api/api/views.py
@@ -5,19 +5,18 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
 from .services import ApiService
-from .serializers import CallsApiSerializer, CallUploadSerializer
+from .serializers import CallsApiSerializer, CallsSerializer
 import json
 
-class CallProcess(APIView):
-    @staticmethod
-    def post(request):
-        serializer = CallsApiSerializer(data=request.data)
-        # serializer.is_valid(raise_exception=True)
-        # serializer.save()
-        api = ApiService(request.data)
+
+class CallProcess(generics.ListCreateAPIView):
+
+    def post(self, request):
+        CallsApiSerializer(data=request.data)
+        data = json.loads(request.data['data'])
+        api = ApiService(data)
         api.save_start_call()
-        # price = api.calculate_bills()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data, status=status.HTTP_201_CREATED)
     #
     # @staticmethod
     # def put(request):
@@ -43,12 +42,12 @@ class CallStop(APIView):
 
 
 class DataUploadView(generics.ListCreateAPIView):
-    serializer_class = CallUploadSerializer
+    serializer_class = CallsSerializer
     http_method_names = ['post']
 
     # data_dict = request.data
     def post(self, request):
-        serializer = CallsApiSerializer(data=request.data)
+        serializer = CallsSerializer(data=request.data)
         # serializer.is_valid(raise_exception=True)
         # serializer.save()
         data = json.loads(request.data['data'])
