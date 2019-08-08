7998842cc7a7aac9526128557a59ffb658d5d458
bsbeirao <bsbeirao@gmail.com>
2019-08-07 22:15:43 -0300

    Test settings

--- django_api/settings.py
+++ django_api/settings.py
@@ -14,8 +14,7 @@ import os
 import django_heroku
 import environ
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -161,3 +160,6 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.JSONParser',
     ],
 }
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
