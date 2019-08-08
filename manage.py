fddfa51834623a058813d48c0dc7c2b0ebd229ca
bsbeirao <bsbeirao@gmail.com>
2019-08-07 21:21:26 -0300

    Test settings

--- manage.py
+++ manage.py
@@ -3,10 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    sys.path.append(BASE_DIR)
+    # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # sys.path.append(BASE_DIR)
     os.environ['DJANGO_SETTINGS_MODULE'] = 'django_api.settings'
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_api.settings")
+    # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_api.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
