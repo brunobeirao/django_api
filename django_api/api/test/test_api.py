f7f9960816605254e6a4bc28191ce82ba50eb72f
bsbeirao <bsbeirao@gmail.com>
2019-08-12 11:02:38 -0300

    Improvements and fix

--- django_api/api/test/test_api.py
+++ django_api/api/test/test_api.py
@@ -1,52 +1,48 @@
 import os
 import json
 from pathlib import Path
-from django.test import TestCase
 
+from django.test import TestCase
 from django_api.api.models import Charges
-from rest_framework import status
 
 BASEDIR = Path(__file__).parents[2]
 RESOURCE_DIR = os.path.join(BASEDIR, 'resources')
-# settings.configure(default_settings=django_settings)
-# settings.configure()
 
 
 class TestRequests(TestCase):
 
     def setUp(self):
+        self.url_process = '/api/v1/calls/process'
+        self.url_call = '/api/v1/calls/call/'
         self.url_charge = '/api/v1/calls/charge'
-        self.charge_dict = {
-            'standing_charge': 0.9,
-            'call_charge': 0.3,
+        self.json_to_open = 'test.json'
+        self.charge = {
+            'standing_charge': 0.36,
+            'call_charge': 0.09,
             'useful_day': 16,
             'status': 1
         }
-        charge = Charges(**self.charge_dict)
+        charge = Charges(**self.charge)
         charge.save()
 
-        self.datas = {'data': {
-            'call_id': 345,
-            'start': {
-                'type': 'start',
-                'record_timestamp': '2017-12-13T03:57:13',
-                'source': 999885264,
-                'destination': 99334688
-            },
-            'stop': {
-                'type': 'stop',
-                'record_timestamp': '2017-12-14T20:10:56'
-            }
-        }}
-
     def test_post_process(self):
-        self.url = '/api/v1/calls/process'
-        json_to_open = 'test.json'
-        with open(os.path.join(RESOURCE_DIR, json_to_open)) as json_file:
+        with open(os.path.join(RESOURCE_DIR, self.json_to_open)) as json_file:
             data = json.load(json_file)
-            response = self.client.post(self.url, json.dumps(data), content_type='application/json')
+            response = self.client.post(self.url_process, json.dumps(data), content_type='application/json')
             self.assertEqual(response.data, data[7])
+
+    def test_get_call(self):
+        with open(os.path.join(RESOURCE_DIR, self.json_to_open)) as json_file:
+            data = json.load(json_file)
+            response = self.client.post(self.url_process, json.dumps(data), content_type='application/json')
+        call_id = response.data['call_id']
+        response = self.client.get(self.url_call + str(call_id))
+        self.assertEqual(response.data[0]['id'], call_id)
 
     def test_get_charges(self):
         response = self.client.get(self.url_charge)
-        self.assertEqual(response.data[0]['status'], self.charge_dict['status'])
+        self.assertEqual(response.data[0]['status'], self.charge['status'])
+
+    def test_post_charges(self):
+        response = self.client.post(self.url_charge, json.dumps(self.charge), content_type='application/json')
+        self.assertEqual(response.data['status'], self.charge['status'])
--- django_api/api/views.py
+++ django_api/api/views.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, generics
